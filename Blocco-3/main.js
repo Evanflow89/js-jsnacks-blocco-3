ba6a876c95c0c3a283b4a006b7f3d9a5d87f5568
Evanflow89 <ivanbaragone@gmail.com>
2022-05-05 16:49:58 +0200

    commit

--- Blocco-3/main.js
+++ Blocco-3/main.js
@@ -7,4 +7,19 @@ const greenDiv = document.getElementById("green");
 redDiv.style.color = "red";
 greenDiv.style.color = "green";
 
-// Partendo da un array di numeri, stampiamo nell’id rosso i numeri dispari e in verde i numeri pari.
+// Partendo da un array di numeri, stampiamo nell’id rosso i numeri pari e in verde i numeri dispari.
+
+
+const Numbers = [12, 24, 11, 239, 68, 932, 65, 222, 7545, 8, 9, 26];
+const evenNum = [];
+const oddNum = [];
+
+for (let i = 0; i < Numbers.length; i++) {
+    if (Numbers[i] % 2 === 0) {
+        evenNum.push(Numbers[i]);
+        redDiv.innerHTML = evenNum;
+    } else {
+        oddNum.push(Numbers[i]);
+        greenDiv.innerHTML = oddNum;
+    }
+}
