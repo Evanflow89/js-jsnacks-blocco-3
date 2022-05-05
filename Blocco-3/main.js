8de81534ad71093af364fa60e4e15d7757c70ccc
Evanflow89 <ivanbaragone@gmail.com>
2022-05-05 16:33:30 +0200

    add style

--- Blocco-3/main.js
+++ Blocco-3/main.js
@@ -1,3 +1,10 @@
 // Crea due tag div con due id diversi:
+const redDiv = document.getElementById("red");
+const greenDiv = document.getElementById("green");
+
 // un div avrà il testo colorato di rosso mentre l’altro di verde.
+
+redDiv.style.color = "red";
+greenDiv.style.color = "green";
+
 // Partendo da un array di numeri, stampiamo nell’id rosso i numeri dispari e in verde i numeri pari.
